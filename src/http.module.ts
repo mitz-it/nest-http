8d8d67e70e2c1328901b5b384f60c12e8e01a5e1
lgcmotta <lgcmotta01@gmail.com>
2023-10-07 17:09:24 -0300

    fix: remove http.service and forRoot from module

--- src/http.module.ts
+++ src/http.module.ts
@@ -1,26 +1,9 @@
 import { Module, DynamicModule } from '@nestjs/common';
-import { HttpService } from './http.service';
 import { CreateAxiosDefaults } from 'axios';
 import { HttpClientFactory } from './http-factory.service';
 
 @Module({})
 export class HttpModule {
-  static factory: HttpClientFactory;
-
-  static forRoot(urls: { name: string; value: string }[]): DynamicModule {
-    const httpService = new HttpService(urls);
-    return {
-      module: HttpModule,
-      providers: [
-        {
-          provide: HttpService,
-          useValue: httpService,
-        },
-      ],
-      exports: [HttpService],
-    };
-  }
-
   static register(
     configs: { name: string; config: CreateAxiosDefaults<any> }[],
   ): DynamicModule {
